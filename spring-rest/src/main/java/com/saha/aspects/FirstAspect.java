205f2ceece539ff9d50b028874c6e9f0d5d761b7
Deniz Silahcılar <deniz.silahcilar@PT0001.local>
2016-02-13 12:21:23 +0200

    first aspect (before)

---
+++ spring-rest/src/main/java/com/saha/aspects/FirstAspect.java
@@ -0,0 +1,19 @@
+package com.saha.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class FirstAspect {
+
+    @Before("execution(* com.saha..*Service.*(..))")
+    public void first(JoinPoint jp) {
+        log.info("Aspectim calisti" + jp.toString());
+    }
+
+}
