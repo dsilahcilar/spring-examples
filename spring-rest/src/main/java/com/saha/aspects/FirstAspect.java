ac53a9dcd026eaef03215986d47759245de3fe68
Deniz Silahcılar <deniz.silahcilar@PT0001.local>
2016-02-13 12:28:48 +0200

    third aspect (after return)

--- spring-rest/src/main/java/com/saha/aspects/FirstAspect.java
+++ spring-rest/src/main/java/com/saha/aspects/FirstAspect.java
@@ -3,6 +3,7 @@ package com.saha.aspects;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -17,8 +18,10 @@ public class FirstAspect {
         log.info(jp.toString() + " basladi");
     }
 
-    @After("execution(* com.saha..*Service.*(..))")
-    public void after(JoinPoint jp) {
+    @AfterReturning(value = "execution(* com.saha..*Service.*(..))",
+            returning = "returnValue")
+    public void after(JoinPoint jp, Object returnValue) {
+        log.info(returnValue.toString());
         log.info(jp.toString() + " bitti");
     }
 
