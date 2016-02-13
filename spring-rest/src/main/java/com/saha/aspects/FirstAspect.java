e374edd89405a0d3a32bea10f51d002f0a1a3adc
Deniz Silahcılar <deniz.silahcilar@PT0001.local>
2016-02-13 12:24:42 +0200

    second aspect (after)

--- spring-rest/src/main/java/com/saha/aspects/FirstAspect.java
+++ spring-rest/src/main/java/com/saha/aspects/FirstAspect.java
@@ -2,6 +2,7 @@ package com.saha.aspects;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,7 +14,12 @@ public class FirstAspect {
 
     @Before("execution(* com.saha..*Service.*(..))")
     public void first(JoinPoint jp) {
-        log.info("Aspectim calisti" + jp.toString());
+        log.info(jp.toString() + " basladi");
+    }
+
+    @After("execution(* com.saha..*Service.*(..))")
+    public void after(JoinPoint jp) {
+        log.info(jp.toString() + " bitti");
     }
 
 }
