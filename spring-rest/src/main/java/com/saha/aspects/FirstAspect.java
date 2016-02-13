c9db7f061a922d7447fd6435a7d7834ba832c307
Deniz Silahcılar <deniz.silahcilar@PT0001.local>
2016-02-13 14:10:48 +0200

    last aspect (throw and around advices)

--- spring-rest/src/main/java/com/saha/aspects/FirstAspect.java
+++ spring-rest/src/main/java/com/saha/aspects/FirstAspect.java
@@ -2,10 +2,8 @@ package com.saha.aspects;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -13,16 +11,32 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class FirstAspect {
 
-    @Before("execution(* com.saha..*Service.*(..))")
+    // @Before("execution(* com.saha..*Service.*(..))")
     public void first(JoinPoint jp) {
         log.info(jp.toString() + " basladi");
     }
 
-    @AfterReturning(value = "execution(* com.saha..*Service.*(..))",
-            returning = "returnValue")
+    //  @AfterReturning(value = "execution(* com.saha..*Service.*(..))",
+    //        returning = "returnValue")
     public void after(JoinPoint jp, Object returnValue) {
         log.info(returnValue.toString());
         log.info(jp.toString() + " bitti");
+    }
+
+    // @AfterThrowing(value = "execution(* com.saha..*Service.*(..))",
+    //       throwing = "ex")
+    public void throwingAdvice(JoinPoint jp, Exception ex) {
+        log.error("Bir hata olustu", ex.getCause().getMessage());
+        log.error(jp.toString());
+    }
+
+    @Around("execution(* com.saha..*Service.*(..))")
+    public Object aroundAdvice(ProceedingJoinPoint pjp)
+            throws Throwable {
+        log.info("Around calisacak");
+        Object result = pjp.proceed();
+        log.info("Around bitti");
+        return result;
     }
 
 }
